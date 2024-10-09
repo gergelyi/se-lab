18e8325d837f25b26375dadb5217e3b3d8e5654b
unknown <184675@BME.HU>
2024-10-09 17:13:09 +0200

    Added a comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -9,6 +9,7 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
+  // Added generator attribute
   private static Random generator = new Random();
 
   // rate of failing to fire torpedos [0.0, 1.0]
